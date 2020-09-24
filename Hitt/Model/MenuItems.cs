6a775b57343e772abe7c143d736a2febb1feb82f
J <jclark81@student.gsu.edu>
2020-09-24 19:57:21 -0400

    BaseViewModel

---
+++ Hitt/Model/MenuItems.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Hitt.Model
+{
+    public class MenuItems
+    {
+
+        public string Image { get; set; }
+        public string Title { get; set; }
+
+       
+    }
+}
