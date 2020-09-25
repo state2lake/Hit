343ea0dfdfea64deac6483fefec7d2358bf5231d
J <jclark81@student.gsu.edu>
2020-09-24 21:06:16 -0400

    ViewModels

---
+++ Hitt/ViewModel/Base/BaseViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Hitt.Services;
+
+namespace Hitt.ViewModel.Base
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        protected readonly INavigationService NavigationService;
+        protected bool SetProperty<T>(ref T field, T value,[CallerMemberName]string name = null)
+        {
+            bool propertyChanged = false;
+
+            if(!EqualityComparer<T>.Default.Equals(field, value))
+            {
+                field = value;
+                OnPropertyChanged(name);
+                propertyChanged = true;
+            }
+            return propertyChanged;
+        }
+        //protected readonly IDialogService DialogService;
+        //protected readonly IState State;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
---
+++ Hitt/ViewModel/MainMasterDetailViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Hitt.Model;
+using Hitt.Navigation;
+using Hitt.Services;
+using Hitt.View;
+using Hitt.ViewModel.Base;
+using Xamarin.Forms;
+
+namespace Hitt.ViewModel
+{
+    public class MainMasterDetailViewModel : BaseViewModel
+    {
+        public ICommand SelectedCommand => new Command(async () => await SelectedCommandAsync());
+        private ObservableCollection<MenuItems> menuItems;
+        ObservableCollection<MenuItems> MenuItem = new ObservableCollection<MenuItems>();
+
+        public MainMasterDetailViewModel()
+        {
+            AddItemsToMenuItems();
+
+        }
+
+        public async Task SelectedCommandAsync()
+        {
+            await NavigationService.PushAsync(new ExpertTips());
+        }
+        public void AddItemsToMenuItems()
+        {
+            MenuItem.Add(new MenuItems { Title = "Settings" });
+        }
+    }
+}
