f328fef111ba24fed8fd5a53b28988ebceddd799
J <jclark81@student.gsu.edu>
2020-09-24 21:04:47 -0400

    MainMasterDetail page

---
+++ Hitt/Navigation/MainMasterDetailsPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Hitt.Model;
+using Hitt.View;
+using Hitt.ViewModel;
+using Xamarin.Forms;
+
+namespace Hitt.Navigation
+{
+    public partial class MainMasterDetailsPage : MasterDetailPage
+    {
+
+      
+
+        public MainMasterDetailsPage()
+        {
+            InitializeComponent();
+
+            Detail = new NavigationPage(new HomePageView());
+
+            IsPresented = false;
+
+            this.BindingContext = new MainMasterDetailViewModel();
+
+        }
+    }
+}
